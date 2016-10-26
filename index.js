c62961fdce37daccc061bcbe18e736d14816def9
Thallyson Barbosa <thallyson.barbosa123@gmail.com>
2016-10-26 09:18:40 -0300

    Fix bug app

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-var express = require('express');
-var app = express();
+var http = require('http')
+    ,app = require('./config/express')
+    db = require('./config/database');
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+http.createServer(app).listen(3000, function() {
+    console.log('Servidor estutando na porta: ' + this.address().port);
 });
 
