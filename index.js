cba48727204c014564249a20752309f1cdbb5d75
Thallyson Barbosa <thallyson.barbosa123@gmail.com>
2016-10-26 09:52:53 -0300

    Fix index.js

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ http.createServer(app).listen(5000, function() {
     console.log('Servidor estutando na porta: ' + this.address().port);
 });
 
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
